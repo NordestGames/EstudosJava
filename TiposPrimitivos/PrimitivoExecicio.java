6b8b8e8509e135e3466796896f0e70fa25977a85
NordestGames <anailton.af.af@gmail.com>
2025-08-28 12:30:10 -0300

    Armazenar na memória

--- TiposPrimitivos/PrimitivoExecicio.java
+++ TiposPrimitivos/PrimitivoExecicio.java
@@ -15,7 +15,11 @@ public class PrimitivoExecicio {
         float salario = 2500.0F;
         String dataRecebe = "29/08/2025";
 
-        System.out.println("Eu " + nome + " morando no endereço: " + ender + " \nconfirmo que recebi o salário de R$ " + salario + ", na data " + dataRecebe);
-        System.out.printf("Eu %s morando no endereço: %s, %nconfirmo que recebi o salário de R$ %.2f ,na data  %s", nome, ender, salario, dataRecebe);
+        String relatorio = "Eu " +nome+ " morando no endereço: " + ender + ", confirmo que recebi o salário de R$ " + salario + ", na data " + dataRecebe;
+        System.out.println(relatorio);
+
+       // System.out.println("Eu " + nome + " morando no endereço: " + ender + " \nconfirmo que recebi o salário de R$ " + salario + ", na data " + dataRecebe);
+       // System.out.printf("Eu %s morando no endereço: %s, %nconfirmo que recebi o salário de R$ %.2f ,na data  %s", nome, ender, salario, dataRecebe);
+
     }
 }
