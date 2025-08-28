173e4e901248bbc17c8b7cdef23d2f958e5942a8
NordestGames <anailton.af.af@gmail.com>
2025-08-28 12:21:36 -0300

    Estudos

---
+++ TiposPrimitivos/PrimitivoExecicio.java
@@ -0,0 +1,21 @@
+/*
+ Prática
+
+ Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem: 
+
+ Eu <nome> morando no endereço <endereço>,
+ confirmo que recebi o salário de <salário>, na data <data>
+ */
+
+
+public class PrimitivoExecicio {
+    public static void main(String[] args){
+
+        String nome = "Anailton", ender = "Rua dos Bobos";
+        float salario = 2500.0F;
+        String dataRecebe = "29/08/2025";
+
+        System.out.println("Eu " + nome + " morando no endereço: " + ender + " \nconfirmo que recebi o salário de R$ " + salario + ", na data " + dataRecebe);
+        System.out.printf("Eu %s morando no endereço: %s, %nconfirmo que recebi o salário de R$ %.2f ,na data  %s", nome, ender, salario, dataRecebe);
+    }
+}
---
+++ TiposPrimitivos/VariaveisConstantes.java
@@ -0,0 +1,27 @@
+
+public class VariaveisConstantes {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+
+        int idade = 10;
+        long numeroGrande = 100000;
+
+        double salarioDouble= 2000.0;
+        float salarioFloat = 2500.0F;
+
+        byte idadeByte = 28;
+        short idadeShort = 28;
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        char caractere = 'A';
+        String nome = "Goku";
+
+        System.out.println("Sua idade é: " + idade + " anos");
+        System.out.println(numeroGrande);
+        System.out.println("Olá, meu nome é " + nome);
+
+    }
+    
+}
