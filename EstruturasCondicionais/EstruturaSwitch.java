eacf3859db9dc2267873ccb4a34fb1fdf65b2218
NordestGames <anailton.af.af@gmail.com>
2025-09-26 10:29:53 -0300

    Aula de Switch

---
+++ EstruturasCondicionais/EstruturaSwitch.java
@@ -0,0 +1,47 @@
+package EstruturasCondicionais;
+
+public class EstruturaSwitch {
+    public static void main(String[] args) {
+        // Imprimir o dia da semana, considerando 1 como domingo
+        byte dia = 5;
+        
+        // valores que switch pode receber char, int, byte, short, enum, String
+        switch(dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.err.println("Opção inválida");
+                break;
+        }
+
+        // Imprima se a pessoa é homem ou mulher baseado no sexo M ou F
+        char sexo = 'M';
+        switch(sexo){
+            case 'M':
+                System.err.println("Masculino");
+            case 'F':
+                System.err.println("Feminino");
+            default:
+                System.out.println("Inválido");
+        }
+    }
+}
