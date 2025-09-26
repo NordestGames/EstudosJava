2fe28dea04100c074ba13377b1b98d1d37626248
NordestGames <anailton.af.af@gmail.com>
2025-09-26 10:50:13 -0300

    Exercício com switch

---
+++ Exercicios/Exercicio02.java
@@ -0,0 +1,28 @@
+package Exercicios;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        // Utilizando switch e dados os valores de 1 a 7, Imprima se é dia útil ou final de semana
+        // Considerando 1 como domingo
+
+        byte dia = 1;
+
+        switch(dia){
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.err.println("Opção inválido");
+                break;
+            }
+
+    }
+}
