1b79b7e3b17004ef7b4c7f5f98b66325d623ddc4
NordestGames <anailton.af.af@gmail.com>
2025-10-02 19:30:00 -0300

    feat: Exercício utilizando break

---
+++ Exercicios/Exercicio04.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+public class Exercicio04 {
+    // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado 
+    // Condição valorParcela >= 1000
+    public static void main(String[] args) {
+        double valorCarro = 40000;
+
+        for(int parcela = 1; parcela <= valorCarro; parcela++){
+            double valorParcela = valorCarro / parcela;
+
+            if(valorParcela < 1000){
+                break;
+            }
+            System.out.printf("Parcela: %d R$ %.2f\n", parcela, valorParcela);
+        }
+    }
+}
