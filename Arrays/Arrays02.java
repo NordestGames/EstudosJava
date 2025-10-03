0a49bd51371f7e15e6c8084b9a331584f2642ecf
NordestGames <anailton.af.af@gmail.com>
2025-10-02 22:15:16 -0300

    Feat: Valores padrões para primitivos e referências

---
+++ Arrays/Arrays02.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        // Valor padrão de 0 para:byte, short, int, long, float double 0
+        // char: '\u0000' ' '
+        // boolean: false
+        // String: null
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
