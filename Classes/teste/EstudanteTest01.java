de883b032d28cb5bf3f62eab141da3c5a355d7d0
NordestGames <anailton.af.af@gmail.com>
2025-10-24 12:03:07 -0300

    feat: adicionar classes

---
+++ Classes/teste/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package Classes.teste;
+
+import Classes.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 12;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
