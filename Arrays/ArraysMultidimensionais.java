b94896112c03a4d351711d0f0c56839e7a12c289
NordestGames <anailton.af.af@gmail.com>
2025-10-24 11:27:23 -0300

    feat: Arrays Multidimensionais

---
+++ Arrays/ArraysMultidimensionais.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class ArraysMultidimensionais {
+    public static void main(String[] args) {
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 31;
+        dias[1][2] = 31;
+
+        for(int i = 0; i < dias.length; i++){
+            for(int j = 0; j < dias[i].length; j++){  
+                System.out.println(dias[i][j]);
+            }
+        }
+
+        // Foreach
+        for(int[] arrBase : dias){
+            for(int num : arrBase){
+                System.out.println(num);
+            }
+        }
+
+
+    }
+}
