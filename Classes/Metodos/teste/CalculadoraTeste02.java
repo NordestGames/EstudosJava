be11ec80bd242dd4d37a36e20209879cfee9970a
NordestGames <anailton.af.af@gmail.com>
2025-10-26 10:27:41 -0300

    feat: parâmetros

---
+++ Classes/Metodos/teste/CalculadoraTeste02.java
@@ -0,0 +1,11 @@
+package Classes.Metodos.teste;
+
+import Classes.Metodos.dominio.Calculadora;
+
+public class CalculadoraTeste02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+        
+    }
+}
