a636e7bdfd81325beac47d37fd8f0464af98daf0
NordestGames <anailton.af.af@gmail.com>
2025-10-26 10:18:23 -0300

    feat: introdução a metodos

---
+++ Classes/Metodos/teste/CalculadoraTeste.java
@@ -0,0 +1,11 @@
+package Classes.Metodos.teste;
+
+import Classes.Metodos.dominio.Calculadora;
+
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
